d3ba5f4b03505356d03d52241ebbb113796df186
Tomaz Zaman <tzaman@strange.local>
2025-03-15 18:32:11 +0100

    Working (and stable) DDR configuration

--- plat/nxp/soc-ls1046a/gateway_dk/platform.mk
+++ plat/nxp/soc-ls1046a/gateway_dk/platform.mk
@@ -17,17 +17,18 @@ DDRC_NUM_DIMM		:= 0
 NUM_OF_DDRC			:= 1
 DDRC_NUM_CS			:= 1
 DDR_ECC_EN			:= yes
-DDR_DEBUG			:= yes
+DDR_DEBUG			:= no
+DEBUG				:= 1
 
 # On-Board Flash Details
-QSPI_FLASH_SZ		:=	0x20000000
-NOR_FLASH_SZ		:=	0x20000000
+QSPI_FLASH_SZ		:= 0x20000000
+NOR_FLASH_SZ		:= 0x20000000
 
 # Platform specific features.
-WARM_BOOT			:=	no
+WARM_BOOT			:= no
 
 # Adding Platform files build files
-BL2_SOURCES	+=	${BOARD_PATH}/ddr_init.c ${BOARD_PATH}/platform.c
+BL2_SOURCES			+= ${BOARD_PATH}/ddr_init.c ${BOARD_PATH}/platform.c
 
 # Adding platform board build info
 include plat/nxp/common/plat_make_helper/plat_common_def.mk
