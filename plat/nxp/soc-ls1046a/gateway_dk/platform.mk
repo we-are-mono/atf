553886ade684e15ebff12e7bb90b45ed44d04613
RinkeMono <rinke@mono.si>
2025-05-14 16:42:26 +0200

    fix supported boot modes

--- plat/nxp/soc-ls1046a/gateway_dk/platform.mk
+++ plat/nxp/soc-ls1046a/gateway_dk/platform.mk
@@ -8,7 +8,7 @@
 BOOT_MODE			?=	qspi
 BOARD				:=	gateway_dk
 POVDD_ENABLE		:=	no
-SUPPORTED_BOOT_MODE	:=	qspi,emmc
+SUPPORTED_BOOT_MODE	:=	qspi emmc
 
 # DDR Compilation Configs
 CONFIG_STATIC_DDR	:= 1
